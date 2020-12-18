fb9c15f7378c1263f4ee3d9dbd9db10550bd9329
Jatin Prakash <jatinprakash1511@gmail.com>
2020-12-18 18:42:17 +0530

    add data and training

--- nn.py
+++ nn.py
@@ -1,4 +1,20 @@
 import numpy as np
+from tqdm import tqdm
+from utils import softmax
+
+def data_loader(data, batch_size):
+    """Takes in data array and yields batch_size arrays
+    after shuffling data
+    """
+    np.random.shuffle(data)
+    len_data = data.shape[0]
+    num_batches = len_data // batch_size
+    if len_data % batch_size:
+        num_batches += 1
+    
+    for i in range(num_batches):
+        yield data[i * batch_size : (i+1) * batch_size, : ]
+
 
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
@@ -7,6 +23,15 @@ def squared_loss(y_hat, y):
     assert y_hat.shape == y.shape
     loss = (y_hat - y)**2
     return np.sum(loss)
+
+def ReLu(x):
+    return np.maximum(x, 0)
+
+def ReLu_prime(x):
+    x = x.copy()
+    x[x<=0] = 0.
+    x[x>0] = 1.
+    return x
 
 #Derivative of the sigmoid function
 def sigmoid_prime(x):
@@ -30,8 +55,8 @@ class NueralNet:
 
         # Create weight matrixes and output nuerons
         for i in range(1, len(layer_struct)):
-            self.parameters['W'+str(i)] = np.zeros([self.nn[i-1], self.nn[i]])
-            self.parameters['b'+str(i)] = np.zeros(self.nn[i]) 
+            self.parameters['W'+str(i)] = np.random.randn(self.nn[i-1], self.nn[i]) * 0.01
+            self.parameters['b'+str(i)] = np.random.randn(self.nn[i]) 
         
         self.activation = sigmoid
         self.deriv_activation = sigmoid_prime
@@ -41,6 +66,8 @@ class NueralNet:
     def forward(self, X):
         """ Passes X through the nueral network
         Make sure shape of X is [batch_size, no_of_input_features]
+
+        Returns the logits
         """
         self.parameters['a0'] = X
 
@@ -48,67 +75,113 @@ class NueralNet:
             self.parameters['z'+str(i)] = np.dot(self.parameters['a'+str(i-1)], self.parameters['W'+str(i)]) + self.parameters['b'+str(i)]
             self.parameters['a'+str(i)] = self.activation(self.parameters['z'+str(i)])
 
-        return self.parameters['a' + str(len(self.nn)-1)]
+        # Take softmax of last layer
+        self.parameters['a'+str(self.L)] = softmax(self.parameters['z'+str(self.L)], axis=1)
 
-    def backward(self, y):
+        # Return only the logits
+        return self.parameters['z' + str(self.L)]
+
+    def backward(self, y_gt):
         """ Applies the backpropogation on the current `aL` neurons and `y` label.
         Gradients are collected and summed. Make sure to call self.clear_grad to set grads to zero before
         calling on new batch.
         """
-        assert y.shape[0] == self.batch_size
-        assert y.shape[1] == self.nn[self.L]
+        assert y_gt.shape[0] == self.batch_size
+        assert y_gt.shape[1] == self.nn[self.L]
         
-        self.derivatives['dz' + str(self.L)] = (self.parameters['a' + str(self.L)] - y) * self.deriv_activation(self.parameters['z' + str(self.L)])
+        self.derivatives['dz' + str(self.L)] = (self.parameters['a' + str(self.L)] - y_gt)
         self.derivatives['dW' + str(self.L)] = np.dot(np.transpose(self.parameters['a' + str(self.L - 1)]), self.derivatives['dz' + str(self.L)]) # [nn[L-1], nn[L]] = [1, nn[L-1]]^T * [1, nn[L]]
         self.derivatives['db' + str(self.L)] = self.derivatives['dz' + str(self.L)]
 
         for l in range(self.L - 1, 0, -1):
             self.derivatives['dz' + str(l)] = np.dot(self.derivatives['dz' + str(l + 1)], np.transpose(self.parameters['W' + str(l + 1)])) * self.deriv_activation(self.parameters['z' + str(l)])
-            self.derivatives['dW' + str(l)] = np.dot(np.transpose(self.parameters['a' + str(l)]), self.derivatives['dz' + str(l)])
+            self.derivatives['dW' + str(l)] = np.dot(np.transpose(self.parameters['a' + str(l - 1)]), self.derivatives['dz' + str(l)])
             self.derivatives['db' + str(l)] = self.derivatives['dz' + str(l)]
     
     def step(self):
         """ Does the gradient descent step, by subtracting grads from params
         """
         for l in range(1, self.L + 1):
-            self.parameters['W' + str(l)] -= self.lr * (np.sum(self.derivatives['dW' + str(l)], axis=0) / self.batch_size)
-            self.parameters['b' + str(l)] -= self.lr * (np.sum(self.derivatives['db' + str(l)], axis=0) / self.batch_size)
+            self.parameters['W' + str(l)] -= self.lr * np.mean(self.derivatives['dW' + str(l)], axis=0)
+            self.parameters['b' + str(l)] -= self.lr * np.mean(self.derivatives['db' + str(l)], axis=0)
 
     def zero_grad(self):
-        """Clear out the gradients for a new batch"""
+        """Clear out the gradients"""
         self.derivatives = {}
 
 # Test functions to check NN class
 if __name__ == "__main__":
-    model = NueralNet(layer_struct=[2, 3, 1], batch_size=4, lr=1)
+    X_train = np.load(open("kannada/X_train.npy","rb"))
+    X_train = X_train.reshape(-1, 28*28)
 
-    data = np.array([[0,0,0], [1,0,0], [0,1,0], [1,1,1]])
+    y_train = np.load(open("kannada/y_train.npy","rb"))
+    y_train = y_train.reshape(-1, 1)
 
-    import random
-    for i in range(10000):
-        model.zero_grad()
-        loss = 0
-       
-        X = data[:, :2]
-        y = np.array(data[:, 2])
-        y = y.reshape(-1,1)
+    train_data = np.concatenate([X_train, y_train], axis=1)
+
+    X_test = np.load(open("kannada/X_test.npy","rb"))
+    X_test = X_test.reshape(-1, 28*28)
+
+    y_test = np.load(open("kannada/y_test.npy","rb"))
+    y_test = y_test.reshape(-1, 1)
+
+    test_data = np.concatenate([X_test, y_test], axis=1)
+
+    num_epochs = 50
+    batch_size = 100
+    num_classes = 10
+    lr = 0.005
+    architecture = [784, 100, 10]
+
+    model = NueralNet(layer_struct=architecture, lr=lr, batch_size=batch_size)
+
+    for epoch in range(num_epochs):
+        epoch_loss = 0
+
+        correct = 0
+        total = 0
+
+        for data in data_loader(train_data, batch_size):
+            model.zero_grad()
+
+            X = data[:, :-1]
+            # print(X)
+            y = data[:, -1].ravel()
+
+            # now binary vectorize y
+            y_one_hot = np.eye(num_classes)[y]
+            y_hat = model.forward(X)
+
+            probs = softmax(y_hat, axis=1)
+            preds = np.argmax(y_hat, axis=1).ravel()
+
+            # print(preds.shape, y.shape)
+
+            mask = preds == y
+            correct += sum(mask)
+            total += len(mask)
+
+            model.backward(y_one_hot)
+            model.step()
+
+            epoch_loss += squared_loss(probs, y_one_hot)
+            # import pdb; pdb.set_trace()
+        
+        # print(model.derivatives['dW1'])
+        # import pdb; pdb.set_trace()
+        print("Epoch {} loss : {:.5f}".format(epoch, epoch_loss / train_data.shape[0]), end=' ')
+        print("| accuracy : {:.5f}".format(correct / total), end=' ')
+
+        X = test_data[:, :-1]
+        y = test_data[:, -1].ravel()
+
+        # now binary vectorize y
+        y_one_hot = np.eye(num_classes)[y]
         y_hat = model.forward(X)
 
-        model.backward(y)
-        loss += squared_loss(y_hat, y)
-        
-        model.step()
-        print("Epoch loss : {:.5f}".format(loss))
-    # y = model.forward(x)  
+        preds = np.argmax(y_hat, axis=1).ravel()
+        mask = preds == y
+        correct += sum(mask)
+        total += len(mask)
 
-    y = model.forward(np.array([[0, 0]]))
-    print(y)
-
-    y = model.forward(np.array([[0, 1]]))
-    print(y)
-
-    y = model.forward(np.array([[1, 0]]))
-    print(y)
-
-    y = model.forward(np.array([[1, 1]]))
-    print(y)
+        print("| val accuracy : {:.5f}".format(correct / total))
